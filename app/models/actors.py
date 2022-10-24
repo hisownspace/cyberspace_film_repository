ce8e7b811f4aaf941a3d2d832b81797669368160
David Nash <hisownspace@gmail.com>
2022-10-24 16:49:46 -0400

    create Actor model and film_cast table

--- app/models/actors.py
+++ app/models/actors.py
@@ -1,3 +1,26 @@
+from sqlalchemy import ForeignKey
+from app.models.db import db
+
+film_cast = db.Table(
+  "film_cast",
+  db.Column("film_id", ForeignKey="actors.id"),
+  db.Column("actor_id", ForeignKey="films.id")
+)
+db.Table
+
+
+class Actor(db.Model):
+  __tablename__ = "actors"
+  
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(255), nullable=False)
+  date_of_birth = db.Column(db.Date, nullable=True)
+  date_of_death = db.Column(db.Date, nullable=True)
+
+  filmography = db.relationship("Film",
+                                secondary=film_cast,
+                                back_populates="cast")
+
 all_actors = [
   {"id": 1, "name": "Nicholas Cage" },
   { "id": 2, "name": "Natalie Portman" },
