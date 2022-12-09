4ce3da18cf57e50a5e3a97ee371455b16f0d1fdb
David Nash <hisownspace@gmail.com>
2022-12-08 21:38:59 -0500

    fix type in FilmForm validator message

--- app/forms/FilmForm.py
+++ app/forms/FilmForm.py
@@ -8,7 +8,7 @@ class FilmForm(FlaskForm):
   title = StringField(validators=[DataRequired()])
   year = IntegerField(validators=[NumberRange(1902, datetime.now().year)])
   plot = TextAreaField(validators=[Length(max=2000)])
-  photo_url = StringField(validators=[DataRequired(), Length(max=1000), URL(require_tld=True, message="Photo must ve a valid URL!")])
+  photo_url = StringField(validators=[DataRequired(), Length(max=1000), URL(require_tld=True, message="Photo must be a valid URL!")])
   genre_id = IntegerField()
   castIds = StringField()
 
